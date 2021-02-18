316df493a03b7235b20da00ffe79e79eb69c7f9e
Colin Arel <romans_one16@live.com>
2021-02-18 16:07:36 -0600

    homepage appearance set. time to make links pop and loc

--- src/components/Chronicles.js
+++ src/components/Chronicles.js
@@ -9,16 +9,16 @@ export const Chronicles = () => (
             <section class="pop-up-journal">
                 <div class="journal-page-left">
                     <div class="journal-page-footer-left">
-                        <a href="url">Options</a>
+                        <a href="">Options</a>
                         <p>1</p>
-                        <a href="url">Prev</a>
+                        <a href="">Prev</a>
                     </div>
                 </div>
                 <div class="journal-page-right">
                     <div class="journal-page-footer-right">
-                        <a href="url">Next</a>
+                        <a href="">Next</a>
                         <p>2</p>
-                        <a href="url">Close</a>
+                        <a href="">Close</a>
                     </div>
                 </div>
             </section>
@@ -30,19 +30,19 @@ export const Chronicles = () => (
             {/* NAV */}
             <section class="top-nav">
                 <span class="top-nav-bar">
-                    <i class="fas fa-home"></i>
-                    <i class="fas fa-book"></i>
-                    <i class="fas fa-calendar-alt"></i>
-                    <i class="fas fa-users"></i>
+                    <a href="" class="top-nav-icon"><i class="fas fa-home"></i></a>
+                    <a href="" class="top-nav-icon"><i class="fas fa-book"></i></a>
+                    <a href="" class="top-nav-icon"><i class="fas fa-calendar-alt"></i></a>
+                    <a href="" class="top-nav-icon"><i class="fas fa-users"></i></a>
                     <div class="divider">|</div>
-                    <i class="fas fa-map-marked-alt"></i>
-                    <i class="fas fa-feather-alt"></i>
-                    <i class="fas fa-mountains"></i>
-                    <i class="fas fa-medal"></i>
-                    <i class="fas fa-photo-video"></i>
-                    <i class="fas fa-envelope"></i>
-                    <i class="fas fa-user-cog"></i>
-                    <i class="fas fa-question"></i>
+                    <a href="" class="top-nav-icon"><i class="fas fa-map-marked-alt"></i></a>
+                    <a href="" class="top-nav-icon"><i class="fas fa-feather-alt"></i></a>
+                    <a href="" class="top-nav-icon"><i class="fas fa-mountains"></i></a>
+                    <a href="" class="top-nav-icon"><i class="fas fa-medal"></i></a>
+                    <a href="" class="top-nav-icon"><i class="fas fa-photo-video"></i></a>
+                    <a href="" class="top-nav-icon"><i class="fas fa-envelope"></i></a>
+                    <a href="" class="top-nav-icon"><i class="fas fa-user-cog"></i></a>
+                    <a href="" class="top-nav-icon"><i class="fas fa-question"></i></a>
                 </span>
             </section>
 
@@ -56,8 +56,27 @@ export const Chronicles = () => (
                 </div>
             </section>
 
-            <section class="home-footer">
-
+            {/* BOTTOM NAV */}
+            <section class="bottom-nav">
+                <span class="home-footer">
+                    <a href="">
+                        <div class="footer-nav-radial">
+                            <h3 class="home-footer-nav">Journal</h3>
+                        </div>
+                    </a>
+                    <div class="footer-divider">|</div>
+                    <a href="">
+                        <div class="footer-nav-radial">
+                            <h3 class="home-footer-nav">Schedule</h3>
+                        </div>
+                    </a>
+                    <div class="footer-divider">|</div>
+                    <a href="">
+                        <div class="footer-nav-radial">
+                            <h3 class="home-footer-nav">Social</h3>
+                        </div>
+                    </a>
+                </span>
             </section>
         </div>
 

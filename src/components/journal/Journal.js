bb5fc844288140df6d4647a7996664592f57e342
Colin Arel <romans_one16@live.com>
2021-03-01 17:07:16 -0600

    a lot of css for journal main

--- src/components/journal/Journal.js
+++ src/components/journal/Journal.js
@@ -5,7 +5,15 @@ import "../../assets/css/all.css";
 export const Journal = () => {
     return (
         <section className="journal-main-section">
-            JOURNAL
+            <div className="journal-categories">
+                <div className="journal-cat-left">
+                    <div className="journal-cat-adventures"></div>
+                    <div className="journal-cat-journal"></div>
+                </div>
+                <div className="journal-cat-right">
+                    <div className="journal-cat-goalsAndAchievements"></div>
+                </div>
+            </div>
         </section>
     )
 }

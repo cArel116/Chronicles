cbca81ddb66ddbfd44ff4dffeabd7525d7e479d6
Colin Arel <romans_one16@live.com>
2021-03-01 17:48:43 -0600

    completed initial styling for journal adventure section

--- src/components/journal/Journal.js
+++ src/components/journal/Journal.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Journal.css";
 import "../../assets/css/all.css";
+import { Link } from "react-router-dom";
 
 export const Journal = () => {
     return (
         <section className="journal-main-section">
             <div className="journal-categories">
                 <div className="journal-cat-left">
-                    <div className="journal-cat-adventures"></div>
+                    <div className="journal-cat-adventures">
+                        <div className="adventures-pic-div">
+                            <div className="journal-cat-darken15">
+                                <Link className="journal-h2-button" to="/adventures">Adventures</Link>
+                            </div>
+                        </div>
+                    </div>
                     <div className="journal-cat-journal"></div>
                 </div>
                 <div className="journal-cat-right">
